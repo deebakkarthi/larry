1a28c8afbc6a83076d96c1eed263d41c8b5ae60f
Deebakkarthi C R <dbk@deebakkarthi.com>
2024-01-09 16:50:23 +0530

    Display posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,8 @@ import { api } from " /utils/api";
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 
 export default function Home() {
-    const hello = api.post.hello.useQuery({ text: "from tRPC" });
     const user = useUser();
+    const { data } = api.post.getAll.useQuery();
 
     return (
         <>
@@ -20,6 +20,9 @@ export default function Home() {
                 <div>
                     {!user.isSignedIn && <SignInButton />}
                     {user.isSignedIn && <SignOutButton />}
+                </div>
+                <div>
+                    {data?.map((post) => (<div key={post.id}>{post.content}</div>))}
                 </div>
             </main>
         </>
