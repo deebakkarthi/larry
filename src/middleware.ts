c7f948288d7a1c7baceeda4795b18a81f18f802d
Deebakkarthi C R <dbk@deebakkarthi.com>
2024-01-09 15:29:28 +0530

    Add auth

---
+++ src/middleware.ts
@@ -0,0 +1,9 @@
+import { withClerkMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export default withClerkMiddleware(() => {
+    return NextResponse.next()
+})
+export const config = {
+    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
