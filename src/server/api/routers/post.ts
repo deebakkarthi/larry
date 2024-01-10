3f0fff4cc80e0ca464aafa8d46b64bae2a2c6d7a
Deebakkarthi C R <dbk@deebakkarthi.com>
2024-01-10 18:29:14 +0530

    Display posts

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,9 +1,51 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from " /server/api/trpc";
+import { auth, clerkClient } from "@clerk/nextjs";
+import type { User } from "@clerk/nextjs/server";
+import { throws } from "assert";
+import { TRPCError } from "@trpc/server";
+
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    imageUrl: user.imageUrl,
+    email: user.emailAddresses.at(0) || null,
+    fullName: user.firstName + " " + user.lastName,
+  };
+};
 
 export const postRouter = createTRPCRouter({
-    getAll: publicProcedure.query(({ ctx }) => {
-        return ctx.db.post.findMany();
-    })
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.db.post.findMany({ take: 100 });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+      if (!author)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author for post not found",
+        });
+
+      if (!author.email)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author email not found",
+        });
+      return {
+        post,
+        author: {
+          ...author,
+          email: author.email.emailAddress,
+        },
+      };
+    });
+  }),
 });
